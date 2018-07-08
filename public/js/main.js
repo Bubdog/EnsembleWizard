d1a5ca33f7cfb085d67a9a8cc9cef68696d8870d
Josh Epps <jee30037@uga.edu>
2018-07-08 19:56:54 -0400

    Added the ability to delete outfits in the closet

--- public/js/main.js
+++ public/js/main.js
@@ -124,34 +124,6 @@ $(document).ready(function () {
     //     { "id" : N, "top": ensemble N top file name here, "bottoms": ensemble N bottoms file name here, "shoes" : ensemble N shoes file name here}           
   };
 
-  function ShowEnsembles() {
-    var closet = GetSavedEnsembles();
-    let collectionsDivText = "";
-    // this gets all the saved apparel but we aren't using this yet
-    for (index in closet.ensembles) {
-      upper = closet.ensembles[index].top;
-      lower = closet.ensembles[index].bottoms;
-      skids = closet.ensembles[index].shoes;
-      collectionsDivText += '<div class="saved-outfit">';
-      collectionsDivText += '<div class="saved-top">';
-      collectionsDivText += '<img src="' + upper + '">';
-      collectionsDivText += '</div>';
-
-      collectionsDivText += '<div class="saved-bottom">';
-      collectionsDivText += '<img src="' + lower + '">';
-      collectionsDivText += '</div>';
-
-      collectionsDivText += '<div class="saved-shoes">';
-      collectionsDivText += '<img src="' + skids + '">';
-      collectionsDivText += '</div>';
-
-      collectionsDivText += '<a href="#" class="remove hvr-shutter-in-vertical"><i class="fa fa-times" aria-hidden="true"></i> Remove from Closet</a>';
-      collectionsDivText += '</div>';
-    }
-    document.getElementById('collections').innerHTML = collectionsDivText;
-    var i = 1; // this is just here to put a breakpoint
-  };
-
   // Save ensemble clicked. Requires some type of save button with class="saveEnsemble" (i.e. <img src="images/SaveEnsemble.png" class="saveEnsemble" alt="SaveEnsemble"> )
   $('.saveEnsemble').on('click', function () {
 
